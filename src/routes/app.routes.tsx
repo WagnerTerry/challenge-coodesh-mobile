ab9b4a1a9864e4ebf90ef361b32602a038a0c793
Wagner Terry <goncalveswagner15@gmail.com>
2023-09-28 18:59:06 -0300

    feat: Modificando icones no menu

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -4,16 +4,52 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Home} from '../views/Home';
 import {Search} from '../views/Search';
 import {History} from '../views/History';
+// import Ionic from 'react-native-vector-icons/Ionicons';
+
+ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 const {Navigator, Screen} = createBottomTabNavigator();
+
 
 export function AppRoutes() {
   return (
     <NavigationContainer>
-      <Navigator>
-        <Screen name="Home" component={Home} />
-        <Screen name="Search" component={Search} />
-        <Screen name="History" component={History} />
+      <Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+      >
+        <Screen
+         name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ focused,color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+         />
+        <Screen
+         name="Search"
+          component={Search}
+          options={{
+            tabBarLabel: 'Search',
+            tabBarIcon: ({ focused,color, size }) => (
+              <MaterialCommunityIcons name="magnify" color={color} size={size} />
+            ),
+          }}
+          />
+       <Screen
+         name="History"
+          component={History}
+          options={{
+            tabBarLabel: 'History',
+            tabBarIcon: ({ focused,color, size }) => (
+              <MaterialCommunityIcons name="book" color={color} size={size} />
+            ),
+          }}
+          />
       </Navigator>
     </NavigationContainer>
   );
