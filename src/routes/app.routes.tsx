d15b4fd6445998884d9a2aab738b800d041601ab
Wagner Terry <goncalveswagner15@gmail.com>
2023-09-28 19:14:51 -0300

    refactor: Ajuste de nomes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,55 +1,55 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Home} from '../views/Home';
-import {Search} from '../views/Search';
-import {History} from '../views/History';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Home } from '../views/Home';
+import { History } from '../views/History';
 // import Ionic from 'react-native-vector-icons/Ionicons';
 
- import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Favorites } from '../views/Favorites';
 
 
-const {Navigator, Screen} = createBottomTabNavigator();
+const { Navigator, Screen } = createBottomTabNavigator();
 
 
 export function AppRoutes() {
   return (
     <NavigationContainer>
       <Navigator
-      screenOptions={{
-        headerShown: false
-      }}
+        screenOptions={{
+          headerShown: false
+        }}
       >
         <Screen
-         name="Home"
+          name="Word list"
           component={Home}
           options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ focused,color, size }) => (
+            tabBarLabel: 'Word list',
+            tabBarIcon: ({ focused, color, size }) => (
               <MaterialCommunityIcons name="home" color={color} size={size} />
             ),
           }}
-         />
+        />
         <Screen
-         name="Search"
-          component={Search}
-          options={{
-            tabBarLabel: 'Search',
-            tabBarIcon: ({ focused,color, size }) => (
-              <MaterialCommunityIcons name="magnify" color={color} size={size} />
-            ),
-          }}
-          />
-       <Screen
-         name="History"
+          name="History"
           component={History}
           options={{
             tabBarLabel: 'History',
-            tabBarIcon: ({ focused,color, size }) => (
+            tabBarIcon: ({ focused, color, size }) => (
               <MaterialCommunityIcons name="book" color={color} size={size} />
             ),
           }}
-          />
+        />
+        <Screen
+          name="Favorites"
+          component={Favorites}
+          options={{
+            tabBarLabel: 'Favorites',
+            tabBarIcon: ({ focused, color, size }) => (
+              <MaterialCommunityIcons name="star-box" color={color} size={size} />
+            ),
+          }}
+        />
       </Navigator>
     </NavigationContainer>
   );
